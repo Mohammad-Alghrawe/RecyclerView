716ea3cbc4c1aede08e919988122dd4ced6a77c2
Mohammad <54815458+Mohammad-Alghrawe@users.noreply.github.com>
2020-03-17 23:41:05 +0100

    use the layout inflater to inflate a custom_row.xml file

--- app/src/main/java/com/example/recyclerview/myadapter/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/myadapter/MyAdapter.java
@@ -1,17 +1,27 @@
 package com.example.recyclerview.myadapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recyclerview.R;
+
 public class MyAdapter extends RecyclerView.Adapter {
+
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View customRow = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.custom_row,parent, false);
+
+        MyViewHolder myViewHolder = new MyViewHolder(customRow);
+        return myViewHolder;
+
     }
 
     @Override
