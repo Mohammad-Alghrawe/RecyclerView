c26150d7e17f659cab5eb1c1a98a8684b9ae2623
Mohammad <54815458+Mohammad-Alghrawe@users.noreply.github.com>
2020-03-17 23:33:57 +0100

    added Data to model class

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.recyclerview.modelclass.ModelClass;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<ModelClass> modelClassArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        modelClassArrayList = new ArrayList<>();
+        addItems();
+    }
+
+
+    private void addItems(){
+        try {
+                for (int i = 0; i<20; i++){
+                    modelClassArrayList.add(new ModelClass(
+                            R.drawable.smile,
+                            "Name :" + i,
+                            "Email : " + i
+                    ));
+                }
+
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
