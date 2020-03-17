d05bda41edcba20785e67d8e8d086baa909e0179
Mohammad <54815458+Mohammad-Alghrawe@users.noreply.github.com>
2020-03-18 00:04:47 +0100

    Populating data to adapter class

--- app/src/main/java/com/example/recyclerview/myadapter/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/myadapter/MyAdapter.java
@@ -3,18 +3,28 @@ package com.example.recyclerview.myadapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.modelclass.ModelClass;
 
-public class MyAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
 
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    ArrayList<ModelClass> modelClassArrayList;
+
+    public MyAdapter(ArrayList<ModelClass> modelClassArrayList) {
+        this.modelClassArrayList = modelClassArrayList;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
 
         View customRow = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.custom_row,parent, false);
@@ -25,7 +35,12 @@ public class MyAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        ModelClass modelClass = modelClassArrayList.get(i);
+        myViewHolder.contactPic.setImageResource(modelClass.getContactPic());
+
+        myViewHolder.contactName.setText(modelClass.getContactName());
+        myViewHolder.contactEmail.setText(modelClass.getContactEmail());
 
     }
 
@@ -36,8 +51,16 @@ public class MyAdapter extends RecyclerView.Adapter {
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView contactPic;
+        TextView contactName,contactEmail;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            contactPic = itemView.findViewById(R.id.image_contact);
+            contactName = itemView.findViewById(R.id.contact_name);
+            contactEmail = itemView.findViewById(R.id.contact_email);
+
         }
     }
 }
